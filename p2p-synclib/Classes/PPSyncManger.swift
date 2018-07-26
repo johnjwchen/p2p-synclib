a04d154a843dc7df15a976578ace04ca9483d245
Jiawei Chen <jwchen@yelp.com>
2018-07-26 14:42:31 -0700

    _send()

--- p2p-synclib/Classes/PPSyncManger.swift
+++ p2p-synclib/Classes/PPSyncManger.swift
@@ -11,8 +11,33 @@ import UIKit
 
 public class PPSyncManger: NSObject {
     public static var receiveHandler: (Data) -> Void
-    public static func send(change: Data, handler: ((Data, Bool) -> Void)?) {
+    public static func send(userData: Data, handler: ((Data, Bool) -> Void)?) {
         
     }
     
+}
+
+struct Change {
+    let userData: Data
+    let uuid = NSUUID().UUIDString
+    let timeStamp = Date().timeIntervalSince1970
+}
+
+extension Change {
+    // max
+    func toData() -> Data {
+    }
+    // max
+    init(from data: Data) {
+    }
+}
+
+
+
+extension PPSyncManger {
+    func _send(userData: Data, handler: ((Data, Bool) -> Void)?) {
+        if let change = Change(userData: userData) as? Data {
+            // send the change as Data
+        }
+    }
 }
